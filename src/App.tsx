c5c5737b1c76fccbd6466c9034152ca84b9d7d6e
Ogada Stanley Chinedu <54116871+chineduogada@users.noreply.github.com>
2022-02-04 18:50:27 +0100

    feat(refreshButton): add to UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Heading } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, Heading } from "@chakra-ui/react";
 import { useCallback, useEffect } from "react";
 import Providers from "./Providers";
 import {
@@ -12,22 +12,40 @@ export default function App() {
   const { resource, handleFetchResource } = useFetch<
     GetManyPokemonCharactersReturnType
   >();
-  const handleFetch = useCallback(async () => {
-    const data: GetManyPokemonCharactersReturnType = await getManyPokemonCharacters();
+  const handleFetch = useCallback(() => {
+    handleFetchResource({
+      fetcher: async () => {
+        const data: GetManyPokemonCharactersReturnType = await getManyPokemonCharacters();
 
-    return data;
-  }, []);
+        return data;
+      }
+    });
+  }, [handleFetchResource]);
 
   useEffect(() => {
-    handleFetchResource({ fetcher: handleFetch });
-  }, [handleFetchResource, handleFetch]);
+    handleFetch();
+  }, [handleFetch]);
 
   return (
     <Providers>
       <Box w="100vw" h="100vh" bg="gray.100">
-        <Heading as="h1" p={10}>
-          Cribstack Test
-        </Heading>
+        <Flex alignItems="center">
+          <Heading as="h1" p={10}>
+            Cribstack Test
+          </Heading>
+
+          <Button
+            shadow={`0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+            0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+            0 12.5px 10px rgba(0, 0, 0, 0.06),
+            0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+            0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+            0 100px 80px rgba(0, 0, 0, 0.12)`}
+            onClick={handleFetch}
+          >
+            Refresh
+          </Button>
+        </Flex>
 
         <Grid
           gridTemplateColumns="repeat(auto-fit, minmax(250px, 1fr))"
