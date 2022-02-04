31f3bb090b55c55a1d1c55f2c3c6d26c80006bbb
Ogada Stanley Chinedu <54116871+chineduogada@users.noreply.github.com>
2022-02-04 19:36:34 +0100

    style: customize scrollbar

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,8 +1,29 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      "*": {
+        scrollBehavior: "smooth"
+      },
+
+      "*::-webkit-scrollbar": {
+        width: "6px",
+        height: "6px"
+      },
+      "*::-webkit-scrollbar-thumb": {
+        backgroundColor: "red.500"
+      },
+      "*::-webkit-scrollbar-track": {
+        backgroundColor: "red.200"
+      }
+    }
+  }
+});
 
 const Providers: React.FC = ({ children }) => {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 };
 
 export default Providers;
